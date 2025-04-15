07cae5f7e3816873cd466c55abfcef945ef615c3
TaoTokikawa <taotokikawa@gmail.com>
2025-04-14 17:43:15 -0700

    test message

--- scripts.js
+++ scripts.js
@@ -124,7 +124,7 @@ function editCardContent(card, album) {
     button.textContent = song.songs[i];
     button.classList.add("song-button");
     button.onclick = function(){
-      alert('Clicked ' + song.songs[i]);
+      alert('Clicked 1' + song.songs[i]);
     };
     //li.appendChild(button);
     list.appendChild(button);
